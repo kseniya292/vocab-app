889bab61d9b06e6f12b228ee2d4bf4adb8c0b432
Kseniya Hogan <kseniya.hogan@gmail.com>
2017-06-14 13:48:31 -0400

    created definition component and added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Injectable }    from '@angular/core';
 import { Headers, Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { Word } from './word/word.model';
 import { VocabService } from './vocab.service';
+import { DefinitionComponent } from './definition/definition.component';
+
 
 @Component({
   selector: 'app-root',
@@ -13,22 +14,4 @@ import { VocabService } from './vocab.service';
 
 export class AppComponent {
   title = 'Vocab App';
-  vocabword: string;
-
-  constructor(private _vocabService: VocabService) {}
-
-  // ngOnInit() { this.getDefinition(); }
-
-  getDefinition(vocabword) {
-    this._vocabService.getDefinition(vocabword)
-                      .subscribe();
-  }
-
-  // getDefinition(vocabword: HTMLInputElement): boolean {
-  //   console.log(`The word is ${vocabword.value}`);
-  //   this.words.push(new Word(vocabword.value, 0));
-  //   vocabword.value = '';
-  //   return false;
-  // }
-
 }
--- src/app/vocab.service.ts
+++ src/app/vocab.service.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Injectable }    from '@angular/core';
 import { Headers, Http, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { parseString } from 'xml2js';
 
 import 'rxjs/add/operator/map';
@@ -18,7 +18,6 @@ import 'rxjs/add/operator/catch';
 
 export class VocabService {
   constructor(private http : Http) { }
-  // private dictionaryApi = 'http://www.dictionaryapi.com/api/v1/references/' + vocabword + '/xml/?key=f6b648e8-a858-4f2a-8abf-705db66d8897';
 
 //fetch Word
   getDefinition(vocabword: string) {
@@ -26,11 +25,8 @@ export class VocabService {
         return this.http.get('http://www.dictionaryapi.com/api/v1/references/collegiate/xml/' + vocabword + '?key=f6b648e8-a858-4f2a-8abf-705db66d8897')
                    .map(res => {
                      parseString(res.text(), function (err, result) {
-                       console.log(result)
+                       return result;
                      })
-                    //  let body = res.text()
-                    //  console.log(body);
-                    //  res.json()
                    })
                    .catch((error:any) => Observable.throw(error || 'Server error'));
                  }
